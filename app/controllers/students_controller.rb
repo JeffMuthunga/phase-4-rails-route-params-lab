4e588e51b8e4e6818e56807e0ebb3aa31dcff957
Jeff Muthunga <jeff.wainaina@student.moringaschool.com>
2023-03-14 07:59:05 +0300

    Complete

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,5 +4,13 @@ class StudentsController < ApplicationController
     students = Student.all
     render json: students
   end
+  def show
+    student =  Student.find(params[:id])
+    render json: student
+  end
 
+  def search
+    students = Student.where(first_name: params[:value1] )
+    render json: students
+  end
 end
